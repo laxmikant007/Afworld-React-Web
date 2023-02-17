1c4a3305231d743f6a7ae9e3b6301a29036892d6
LAXMIKANT SARASWAT <laxmikantsarswat007@gmail.com>
2023-02-17 23:27:32 +0530

    login backend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,9 +2,60 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../assets/css/register.css";
 import loginImage from  "../assets/images/login-page.jpg"
+import { useState, useEffect } from "react";
 // import "../assets/css/signin.css";
 
+
+
+
+
+
+
+
+
 export default function Login()  {
+
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+  // const navigate=useNavigate()
+
+    // useEffect(()=>{
+    //     const auth=localStorage.getItem('user')
+    //     if(auth){
+    //         navigate('/')
+    //     }
+    // },[])
+
+
+  const handleClick=  async ()=>{
+    console.log(email,password)
+     let result= await fetch("http://localhost:5000/login",{
+        method:"post",
+        body:JSON.stringify({email,password}),
+        headers:{
+            'Content-Type':'application/json'
+        }
+     }
+     )
+     // the result is in readstream format so it has to be converted into string
+     result= await result.json();
+
+     console.log(result)
+   if(result.auth){
+        //   confirm("Login successfully")
+        localStorage.setItem("user",JSON.stringify(result.user))
+        localStorage.setItem("token",JSON.stringify(result.auth))
+        // navigate('/')
+
+   }
+   else{
+    alert("Please enter correct details")
+   }
+  }
+
+
+
+
   return (
     <div className="container">
      
@@ -14,7 +65,7 @@ export default function Login()  {
           <label for="email">Email Address</label>
           <div class="sec-2">
             <ion-icon name="mail-outline"></ion-icon>
-            <input type="email" name="email" placeholder="Username@gmail.com" />
+            <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" name="email" placeholder="Username@gmail.com" />
           </div>
         </div>
         <div class="input_box">
@@ -26,10 +77,11 @@ export default function Login()  {
               type="password"
               name="password"
               placeholder="············"
+              onChange={(e)=>setPassword(e.target.value)} value={password}
             />
           </div>
         </div>
-         <button class="login"><Link to="/profile" className="link_btn">Login</Link> </button>
+         <button  onClick={handleClick} class="login"   ><Link to="/profile" className="link_btn">Login</Link> </button>
         <div class="footer">
           <Link to="/register" className="links_btn">Sign up</Link>
           <Link className="links_btn">Forgot Password?</Link>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,59 +2,106 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import signupImage from "../assets/images/sign-up.jpg"
 import "../assets/css/register.css";
+import { useState, useEffect } from "react";
 
-export default function Register() {
+
+const Register = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [navigate, setNavigate] = useState('')
+  // const navigate=useNavigate();
+  // const navigate = useNavigate();
+  useEffect(() => {
+    const auth = localStorage.getItem('user')
+    if (auth) {
+      // navigate('/')
+      // <Link to="/"</Link>
+      // setNavigate('/')
+    }
+  }, [])
+  const handleClick = async () => {
+    console.log("function has been called");
+    console.log(name, email, password);
+
+    let result = await fetch("http://localhost:5000/register", {
+      method: "post",
+      body: JSON.stringify({ name, email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+      // navigate('/');
+    // setNavigate('/')
+    // here user is the key
+    localStorage.setItem("user", JSON.stringify(result.result))
+    localStorage.setItem("token", JSON.stringify(result.auth))
+    console.log("result is " + result);
+  };
   return (
     <div className='container' >
-     <div className='section'>
-      <div class="screen-1">
-        <div class="input_box">
-          <label for="name">Full Name</label>
-          <div class="sec-2">
-            <ion-icon name="person-outline"></ion-icon>
-            <input type="text" name="name" placeholder="Enter Your name" />
+      <div className='section'>
+        <div class="screen-1">
+          <div class="input_box">
+            <label for="name">Full Name</label>
+            <div class="sec-2">
+              <ion-icon name="person-outline"></ion-icon>
+              <input value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }} type="text" name="name" placeholder="Enter Your name" />
+            </div>
+          </div>
+          <div class="input_box">
+            <label for="email">Email Address</label>
+            <div class="sec-2">
+              <ion-icon name="mail-outline"></ion-icon>
+              <input value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }} type="email" name="email" placeholder="Username@gmail.com" />
+            </div>
+          </div>
+          <div class="input_box">
+            <label for="number">Mobile number</label>
+            <div class="sec-2">
+              <ion-icon name="call-outline"></ion-icon>
+              <input type="email" name="email" placeholder="+ 91-000-0000-000" />
+            </div>
+          </div>
+          <div class="input_box">
+            <label for="password">Password</label>
+            <div class="sec-2">
+              <ion-icon name="lock-closed-outline"></ion-icon>
+              <input
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                class="pas"
+                type="password"
+                name="password"
+                placeholder="············"
+              />
+            </div>
+          </div>
+          <button onClick={handleClick}  class="login">Sign Up</button>
+          <div class="footer">
+            Already have account? <Link to="/login" className="links_btn">Login</Link>
           </div>
         </div>
-        <div class="input_box">
-          <label for="email">Email Address</label>
-          <div class="sec-2">
-            <ion-icon name="mail-outline"></ion-icon>
-            <input type="email" name="email" placeholder="Username@gmail.com" />
-          </div>
-        </div>
-        <div class="input_box">
-          <label for="number">Mobile number</label>
-          <div class="sec-2">
-            <ion-icon name="call-outline"></ion-icon>
-            <input type="email" name="email" placeholder="+ 91-000-0000-000" />
-          </div>
-        </div>
-        <div class="input_box">
-          <label for="password">Password</label>
-          <div class="sec-2">
-            <ion-icon name="lock-closed-outline"></ion-icon>
-            <input
-              class="pas"
-              type="password"
-              name="password"
-              placeholder="············"
-            />
-          </div>
-        </div>
-        <button class="login">Sign Up</button>
-        <div class="footer">
-          Already have account? <Link to="/login" className="links_btn">Login</Link>
-        </div>
+
+
+      </div>
+      <div class="image-container">
+        <img src={signupImage} alt="Image" style={{ maxWidth: "100%", height: "auto", width: "40vh" }} />
       </div>
 
 
-    </div>
-          <div class="image-container">
-          <img src={signupImage} alt="Image" style={{ maxWidth: "100%", height: "auto" , width:"40vh" }}/>
-          </div>
 
-
-    
     </div>
   )
 }
+export default Register;
